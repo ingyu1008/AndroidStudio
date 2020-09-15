0c3b54b3fe1fa4e416698c9350df03d682ad53a4
ingyu1008 <ingyu1008@gmail.com>
2020-09-15 12:43:13 +0900

    ADD : RestCall.java

--- CodeforcesCalender/app/src/main/java/com/example/codeforcescalender/RestCall.java
+++ CodeforcesCalender/app/src/main/java/com/example/codeforcescalender/RestCall.java
@@ -1,4 +1,31 @@
 package com.example.codeforcescalender;
 
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class RestCall {
+    public static void restCall(String paramUrl){
+        try {
+            URL url = new URL(paramUrl);
+            HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(),"UTF-8"));
+            if (conn.getResponseCode() != 200) {
+                System.out.println("Failed: HTTP error code : " + conn.getResponseCode());
+                throw new RuntimeException("Failed: HTTP error code : " + conn.getResponseCode());
+            } else {
+                System.out.println("발송 성공");
+            }
+
+            String line = null;
+            while((line = br.readLine()) != null){
+                System.out.println(line);
+            }
+            br.close();
+            conn.disconnect();
+        } catch (IOException e) {
+            System.out.println("RestCall Fail : " + e.getMessage());
+        }
+    }
 }
